514966e3418b86a4e933d368176fb8287f6c5c62
memo figueredo <memof80@gmail.com>
2021-03-15 11:05:26 -0500

    Setup Project

---
+++ AppStoreJsonClone/BaseTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreJsonClone
+//
+//  Created by Memo Figueredo on 15/3/21.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+    }
+}
---
+++ AppStoreJsonClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreJsonClone
+//
+//  Created by Memo Figueredo on 15/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
