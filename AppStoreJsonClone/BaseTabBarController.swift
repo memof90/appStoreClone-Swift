fd57ca01c1008190559d1ca165f8449c726adc67
memo figueredo <memof80@gmail.com>
2021-03-15 12:25:27 -0500

    created principla interface

--- AppStoreJsonClone/BaseTabBarController.swift
+++ AppStoreJsonClone/BaseTabBarController.swift
@@ -12,19 +12,34 @@ class BaseTabBarController: UITabBarController {
         super.viewDidLoad()
         
         let redViewController = UIViewController()
-        redViewController.view.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-        redViewController.tabBarItem.title = "Red"
+        redViewController.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        redViewController.navigationItem.title = "Apps"
         
         
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "Apps"
+        redNavController.navigationBar.prefersLargeTitles = true
+        redNavController.tabBarItem.image = #imageLiteral(resourceName: "apps")
         
         let blueViewController = UIViewController()
-        blueViewController.view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-        blueViewController.tabBarItem.title = "Blue"
+        blueViewController.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        blueViewController.navigationItem.title = "Search"
         
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "Search"
+        blueNavController.navigationBar.prefersLargeTitles = true
+        blueNavController.tabBarItem.image = UIImage(named: "search")
         
+//        MARK: CHANGE COLOR TABBAR
+//        tabBar.barTintColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+//        MARK: CHANGE COLOR ICON TABBAR
+//        tabBar.tintColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+       
+         
         viewControllers = [
-            redViewController,
-            blueViewController,
+            redNavController,
+            blueNavController,
         ]
     }
 }
