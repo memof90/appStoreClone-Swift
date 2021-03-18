418a8fa3a94872ff40aea95225a6acb79ef9fb66
memo figueredo <memof80@gmail.com>
2021-03-18 17:44:50 -0500

    created fetchItunesData

--- AppStoreJsonClone/Controller/AppsSearchController.swift
+++ AppStoreJsonClone/Controller/AppsSearchController.swift
@@ -19,6 +19,49 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         
 //        allow to register identifier cell
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
+//        Fuction to pass data Api Itunes
+        fetchItunesApps()
+    }
+    
+//    Create struct but pass to other docuemnts
+    struct SearchResult: Decodable {
+        let resultCount: Int
+        let results: [Result]
+    }
+    
+    struct Result: Decodable {
+        let trackName: String
+        let primaryGenreName: String
+    }
+    
+    fileprivate func fetchItunesApps(){
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        
+        guard let url = URL(string: urlString) else { return }
+//        fetch data to internet
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+//            error to requets
+            if let err = err {
+                print("Failed to fetch apps:", err)
+                return
+            }
+            
+//            sucess
+//            print(data)
+//            print(String(data: data!, encoding: .utf8))
+            guard let data = data else { return }
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+//                print(searchResult)
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+            } catch let jsonErr {
+                debugPrint("Failed to decode json:", jsonErr)
+            }
+
+            
+            
+        }.resume() //fires off the requets dispara la solicitud
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -32,7 +75,8 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = ""
         return cell
     }
     
